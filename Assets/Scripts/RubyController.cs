b1104e05e093e858e7ce524c7531606ded5bee44
Nunozki <nuno.miguel99@hotmail.com>
2023-03-03 12:29:44 +0000

    2nd Commit: Character Controller and Keyboard Input

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
 
 public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
-
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        //Debug.Log(horizontal);
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        //position.x = position.x + 0.01f;
-        //position.x = position.x + 0.1f;
-        position.x = position.x + 0.3f;
+        //position.x = position.x + 0.1f
+        //position.x = position.x + 0.3f
+        //position.x = position.x + 0.01f
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
